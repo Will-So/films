ef505314b6a072c1d270e1a140c25088cf9be7ae
Will Sorenson <ryan.d.sorenson@gmail.com>
2015-10-05 10:58:44 -0400

    minor changes

--- imdb_scrape.py
+++ imdb_scrape.py
@@ -1,1 +1,17 @@
-__author__ = 'Will'
+"""
+Bias Reduction:
+    - The wikipedia list has 1400 movies rather than 1000 - 200 = 800. I may want to end up
+    using that ne instead
+Strategy:
+1.
+"""
+import pandas as pd
+
+def load_sequel_titles():
+    """
+
+    :return:
+    """
+    df = pd.read_csv('movies_with_sequels.csv')
+    del df['position']
+
