5db2c9cbcbf8f266b9b5a0ac5cc706a4ecbf8395
Will Sorenson <ryan.d.sorenson@gmail.com>
2015-10-07 09:36:53 -0400

    everything but extracting attributes done

--- movie_mojo_scraper.py
+++ movie_mojo_scraper.py
@@ -18,28 +18,49 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import string
+import pickle
+
+pickle_everything = True
 
 
-def get_movie_id_list():
+def get_movie_urls():
     """
+    Collects the URL of every movie listed on box office mojo
+
     :return: List of all movie ids on the Beautiful Soup website
     """
 
     letter_urls = iterate_letters()
     category_urls = iterate_letter_category(letter_urls)
-    movie_ids = []
+    movie_urls = []
     for url in category_urls:
-        pass
-        # Adds the movie ids to all the lists in the thing
+        text = requests.get(url).text
+        soup = BeautifulSoup(text)
+        soup = soup.find_all('table')[3]
 
-    return movie_ids
+        for link in soup.find_all('a'):
+            if 'schedule' in link.attrs['href']:
+                continue # Removes the date hrefs
+
+            movie_urls.append('http://www.boxofficemojo.com' + link.get('href'))
+        print("Finished the following Category ", url)
+
+
+    return movie_urls
 
 
 def iterate_letters():
     """
+    Helper function for get_movie_urls. Returns the url for each letter.
 
     :param letter:
     :return: List of the URLs for each letter
+
+    Examples
+    ---
+    >>> iterate_letters()[:2]
+     ['http://www.boxofficemojo.com/movies/alphabetical.htm?letter=a',
+        'http://www.boxofficemojo.com/movies/alphabetical.htm?letter=b']
     """
 
     letter_urls = ['http://www.boxofficemojo.com/movies/alphabetical.htm?letter=' + i for
@@ -54,14 +75,19 @@ def iterate_letters():
 
 def iterate_letter_category(letter_urls):
     """
-    Returns a list of all the letter category URLs. For example,
+    Helper function for  get_movie_urls. Returns a list of all the letter category URLs.
 
     :param letter:
     :return: List of the URL of letter categories
 
     Examples
     ---
-    >>>
+    >>> iterate_letter_category(iterate_letters())[:4]
+    ['http://www.boxofficemojo.com/movies/alphabetical.htm?letter=a',
+    'http://www.boxofficemojo.com/movies/alphabetical.htm?letter=A&page=2&p=.htm',
+    'http://www.boxofficemojo.com/movies/alphabetical.htm?letter=A&page=3&p=.htm',
+    'http://www.boxofficemojo.com/movies/alphabetical.htm?letter=A&page=4&p=.htm']
+
     """
     letter_category_urls = []
     for url in letter_urls:
@@ -79,15 +105,23 @@ def parse_movies(movie_list):
     """
     Takes a list of movies and returns all pertinant data in the form of a Pandas Dataframe
 
+    Extracts the following Data:
+    1. Domestic Total Gross
+    2. Genres
+    3. MPAA ratings
+    4. Runtime
+    5. Production Budget
+    6. Distributor
+    7. Release Date
+    8. Actors
+
 
     :param movie_list: List of movie titles
     :return: Pandas Dataframe of
     """
     cleaned_data = pd.DataFrame()
 
-    for movie in movie_list:
-        url = "http://www.boxofficemojo.com/movies/?page=weekly&id=" + movie + ".htm"
-
+    for url in movie_list:
         request = requests.get(url)
         soup = BeautifulSoup(request.text)
         cleaned_data.append(retrieve_attributes(soup))
@@ -112,6 +146,8 @@ def retrieve_attributes(soup):
 
 
 def main():
-    movie_list = get_movie_id_list()
+    movie_list = get_movie_urls()
+    if pickle_everything:
+        pickle.dump(movie_list, 'movie_list.pkl')
     movie_data = parse_movies(movie_list)
     movie_data.to_csv('/Data/films/mojo.csv')
